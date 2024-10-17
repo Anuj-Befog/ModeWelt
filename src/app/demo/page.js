8c7bd95205a07b2fe59316eab47b933cf3021fc4
Anuj3553 <anujverma3553@gmail.com>
2024-10-17 18:42:19 +0530

    Testing of Frontend Login & Signup Page

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -1,140 +1,52 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { gsap } from "gsap";
-import { useEffect } from "react";
-import Image from "next/image";
+import React, { useState } from 'react'
+import axios from 'axios';
+import Link from 'next/link';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
-export default function Demo() {
-    useEffect(() => {
-        const tl = gsap.timeline();
+export default function Profile() {
+    const router = useRouter();
+    const [data, setData] = useState("nothing");
 
-        tl.to(".star", {
-            duration: 0.4,
-            transition: "all 0.5s",
-            stagger: 0.2,
-            opacity: 1,
-            color: "#04AF31",
-            ease: "elastic",
-        })
-            .to("#presuccess", {
-                border: "4px solid #04AF31",
-                duration: 0.3,
-                ease: "linear",
-            }, '>')
-            .to('#stars-container', {
-                border: "4px solid #04AF31",
-                duration: 0.3,
-                ease: "linear",
-            }, '>')
-            .to('#check', {
-                scale: 1,
-                opacity: 1,
-                duration: 0.3,
-                ease: "elastic",
-            }, '>')
-            .to('check', {
-                y: -100,
-                transition: "all 0.2s",
-            })
-            .from('#welcome-heading', {
-                delay: 0.2,
-                y: 20,
-            })
-            .to('#welcome-heading', {
-                y: 0,
-                display: 'block',
-                transition: "all 0.1s",
-                opacity: 1,
-                duration: 0.2,
-                ease: "linear",
-            })
-            .from('#welcome-para', {
-                delay: 0.3,
-                y: 20,
-            })
-            .to('#welcome-para', {
-                y: 0,
-                display: 'block',
-                transition: "all 0.1s",
-                opacity: 1,
-                duration: 0.2,
-                ease: "linear",
-            })
-            .from('#welcome-btn', {
-                delay: 0.5,
-                y: 20,
-            })
-            .to('#welcome-btn', {
-                y: 0,
-                display: 'block',
-                transition: "all 0.1s",
-                opacity: 1,
-                duration: 0.2,
-                ease: "linear",
-            })
-            .from('#check-complete', {
-                x: 100,
-            })
-            .to('#check-complete', {
-                x: 0,
-                display: 'block',
-                transition: "all 0.1s",
-                opacity: 1,
-                duration: 0.2,
-                ease: "power4",
-            })
+    const getUserDetails = async () => {
+        try {
+            const res = await axios.post('/api/users/me');
+            console.log(res.data._id);
+            setData(res.data._id);
+        }
+        catch (error) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
 
-        return () => {
-            tl.kill();
-        };
-    }, []);
+    const logout = async () => {
+        try {
+            await axios.get('/api/auth/logout');
+            toast.success('Logout successfully');
+            router.push('/login');
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
 
     return (
-        <div className="min-h-[88vh] flex flex-col justify-center items-center bg-gray-50">
-            <div className="text-center">
-                <div id="progress" className="flex flex-col items-center justify-center transition transition-300">
-                    <div id="presuccess" className="h-24 w-24 rounded-full border-[4px] border-gray-500">
-                        <div id="check" className="scale-[0.1] opacity-0 relative top-[1.5rem] left-[0.6rem]">
-                            <Image className="w-[4rem]" src='/assets/check.png' width={100} height={100} alt="check" />
-                        </div>
-                    </div>
+        <>
+            <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+                <h1>Profile Page</h1>
+                <hr />
+                <h2>{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}></Link>}</h2>
+                <hr />
 
-                    <div id="stars-container" className="relative mt-4 border-gray-600 border-[3px] w-[200px] h-[39px] text-white py-2 px-8 rounded-full font-semibold">
-                        <div className="stars flex justify-around items-center text-[2.5rem] gap-[10px] text-gray-500 relative bottom-[1.1rem] right-[1.5rem]">
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                            <div className="star">*</div>
-                        </div>
-                        <div id="check-complete" className="absolute mt-4 bottom-[-8%] right-[-2%] hidden bg-green-600 w-[200px] h-[39px] text-white py-2 px-8 rounded-full font-semibold">
-                            COMPLETE
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <button className='bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg' onClick={logout}>Logout</button>
 
-            <div className="text-center mt-12 transition transition-300">
-                <h1 id="welcome-heading" className="text-3xl md:text-5xl font-bold text-primary hidden transition transition-300">
-                    WELCOME BACK! TO MODEWELT
-                </h1>
-                <p id="welcome-para" className="mt-4 uppercase text-sm w-[60%] mx-auto hidden transition transition-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
-                    since the 1500s.
-                </p>
+                <button className='bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg' onClick={getUserDetails}>
+                    Get User Details
+                </button>
             </div>
-
-            <div className="mt-12">
-                <Link href='/'>
-                    <button id="welcome-btn" className="bg-primarybtn hover:bg-primary text-white py-3 px-8 rounded-full font-semibold hidden">
-                        GO TO HOME PAGE
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
+        </>
+    )
 }
---
+++ src/app/testverifyemail/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TestVerifyEmail() {
+    return (
+        <div>
+            <h1>TestVerifyEmail</h1>
+        </div>
+    )
+}
+
