3a72b6ab88246bdc99d585b3fd12dc497b56a83b
Anuj3553 <anujverma3553@gmail.com>
2024-10-23 10:59:04 +0530

    Feed Page Fix

--- src/app/feed/page.js
+++ src/app/feed/page.js
@@ -20,7 +20,7 @@ const Post = () => {
   `;
 
   // Determine the trimmed text version (visible in collapsed state)
-  const trimmedText = text.substring(0, 250); // Show first 180 characters
+  const trimmedText = text.substring(0, 340); // Show first  characters
 
   return (
     <div className="flex justify-center min-h-screen mt-12 space-x-14">
@@ -185,8 +185,8 @@ const Post = () => {
               onError={() => setSrc("/Images/Images1.png")}
             />
             <div className='flex flex-col'>
-              <h1 className="text-[14px] font-[Arial] font-[700]">Aditya Kumar</h1>
-              <span className="text-[10px] font-[Arial] font-[400]">Illustration Designer</span>
+              <h1 className="text-[14px] font-[Arial] font-[700]">Prashant</h1>
+              <span className="text-[10px] font-[Arial] font-[400]">Product designer at Fashionista</span>
             </div>
           </div>
 
@@ -194,24 +194,16 @@ const Post = () => {
           <div className="mt-1 text-gray-600 relative">
             <div className="relative">
               <div
-                className={`px-8 mt-4 text-[14px] transition-all duration-500 ease-in-out overflow-hidden ${expanded ? 'max-h-[1000px]' : 'max-h-[100px]'
+                className={`px-8 mt-4 text-[14px] transition-all duration-500 ease-in-out overflow-hidden max-h-[100px]'
                   }`}
               >
-                {/* Show full or trimmed content based on the expanded state */}
-                {expanded ? text : trimmedText + '...'}
+                How’s your day going, guys?
               </div>
-
-              <button
-                className="px-8 my-2 mt-4 text-[#A45286] text-[12px] font-[Gotham] font-semibold uppercase cursor-pointer transition-all duration-300"
-                onClick={() => setExpanded(!expanded)}
-              >
-                {expanded ? 'Read Less' : 'Read More'}
-              </button>
             </div>
           </div>
 
           {/* Image */}
-          <div className="mt-3">
+          <div className="mt-5">
             <Image
               src="/assets/feedPage/bc.png"
               alt="Post Image"
@@ -265,7 +257,7 @@ const Post = () => {
           </div>
         </div>
 
-        {/* Post Text Section with Content */}
+        {/* Post Text Section with Content & Documents */}
         <div className="bg-white h-[402px] my-4">
           <div className="flex justify-between items-center px-4 py-2">
             <p className="leading-[15px] text-[10px] font-[Gotham] text-[#181818]">
@@ -289,8 +281,8 @@ const Post = () => {
               onError={() => setSrc("/Images/Images1.png")}
             />
             <div className='flex flex-col'>
-              <h1 className="text-[14px] font-[Arial] font-[700]">Aditya Kumar</h1>
-              <span className="text-[10px] font-[Arial] font-[400]">Illustration Designer</span>
+              <h1 className="text-[14px] font-[Arial] font-[700]">Nikhil Gupta</h1>
+              <span className="text-[10px] font-[Arial] font-[400]">Senior Fashion Designer</span>
             </div>
           </div>
 
@@ -335,7 +327,7 @@ const Post = () => {
 
                 {/* Text */}
                 <div className=" space-y-3 px-[0.5rem]">
-                  <p className="ml-4 text-base text-[14px] font-[Arial] font-[400] text-[#181818]">fashion guidelines for Graphic Designer </p>
+                  <p className="ml-4 text-base text-[14px] font-[Arial] font-[400] text-[#181818]">Fashion guidelines for Graphic Designer </p>
                   <span className="pl-4 text-[#181818] text-[10px] leading-[15px] font-[Gotham]">PDF file, 245 kb</span>
                 </div>
 
@@ -382,6 +374,84 @@ const Post = () => {
                   className='w-[13.12px] h-[13.12px]'
                 />
                 <span className="text-[14px] font-semibold font-[Gotham]">79</span>
+              </div>
+              {/* Stats Border */}
+              <div className="absolute w-[1px] left-[11rem] bottom-[-16px] h-[54px] bg-[#F4F4F4]"></div>
+            </div>
+            {/* Share Btn */}
+            <div className="relative flex items-center space-x-2">
+              {/* Stats Border */}
+              <div className="absolute w-[1px] left-[-1.5rem] bottom-[-16px] h-[54px] bg-[#F4F4F4]"></div>
+              <FaShareAlt className="text-primary text-[20px]" />
+              <span className="text-[14px] font-semibold font-[Gotham]">SHARE</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Post Text Section with Content & Documents */}
+        <div className="bg-white h-[259px] my-4">
+          <div className="flex justify-between items-center px-4 py-2">
+            <p className="leading-[15px] text-[10px] font-[Gotham] text-[#181818]">
+              First post from <span className='text-primary'>Audrey Alexander</span>
+            </p>
+            {/* Share Pointers */}
+            <FaEllipsisH className="text-[#181818] hover:text-primary cursor-pointer w-[20px] h-[24px]" />
+          </div>
+
+          {/* Border Top of Post */}
+          <div className="w-[850px] h-[1px] bg-[#F4F4F4] mb-3"></div>
+
+          {/* User Info */}
+          <div className="flex items-center mt-3 space-x-4 px-8">
+            <Image
+              src="/assets/feedPage/profile.png"
+              alt="Profile"
+              width={100}
+              height={100}
+              className="rounded-full w-[52px] h-[52px]"
+              onError={() => setSrc("/Images/Images1.png")}
+            />
+            <div className='flex flex-col'>
+              <h1 className="text-[14px] font-[Arial] font-[700]">Aditya Kumar Kanaujiya</h1>
+              <span className="text-[10px] font-[Arial] font-[400]">Team lead at Fashionista</span>
+            </div>
+          </div>
+
+          {/* Post Description */}
+          <div className=" text-gray-600 mt-4 px-8">
+            <p className='text-[14px] font-[400] leading-[21px] font-[Arial] mb-[1.4rem]'>The bun runs along the road and meets a wolf. «Little bun, little bun, I want to eat you!» says the wolf. «I ran away from Grandfather, I ran away from Grandmother, I ran away from the hare. And I can run away from you, grey wolf!» says the bun and runs away.</p>
+          </div>
+
+          {/* Border Bottom of Post */}
+          <div className="w-[850px] h-[1px] bg-[#F4F4F4] mt-4 mb-3"></div>
+
+          {/* Stats Post */}
+          <div className="flex justify-between items-center px-9 mb-4 mt-4">
+            {/* Stats */}
+            <div className="relative flex gap-[4rem]"> {/* Increased spacing */}
+              {/* Likes */}
+              <div className="flex items-center space-x-2"> {/* Adjusted icon-text spacing */}
+                <Image
+                  height={100}
+                  width={100}
+                  alt='like'
+                  src='/assets/feedPage/like.png'
+                  className='w-[13.12px] h-[13.12px]'
+                />
+                <span className="text-[14px] font-semibold font-[Gotham]">10</span>
+              </div>
+              {/* Stats Border */}
+              <div className="absolute w-[1px] left-[4.4rem] bottom-[-16px] h-[54px] bg-[#F4F4F4]"></div>
+              {/* Comments */}
+              <div className="flex items-center space-x-2"> {/* Adjusted icon-text spacing */}
+                <Image
+                  height={100}
+                  width={100}
+                  alt='like'
+                  src='/assets/feedPage/comment.png'
+                  className='w-[13.12px] h-[13.12px]'
+                />
+                <span className="text-[14px] font-semibold font-[Gotham]">0</span>
               </div>
               {/* Stats Border */}
               <div className="absolute w-[1px] left-[11rem] bottom-[-16px] h-[54px] bg-[#F4F4F4]"></div>
@@ -486,7 +556,7 @@ const Post = () => {
           <p className="font-[600] text-[12px] uppercase leading-[11.48px] font-[Gotham]">Trending Articles</p>
 
           {/* Divider */}
-          <hr className="border-t border-gray-300 mt-4" />
+          <hr className="border-t border-[#F4F4F4] mt-4" />
 
           <div className="w-[227px] h-[198px] mt-2">
             {[1, 2, 3].map((_, index) => (
